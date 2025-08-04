5c8485398d10557a8923baa6248768843e165bcd
Antonwyck Anton <antonwiclyff@gmail.com>
2025-08-04 11:12:45 +0000

    Your commit message here

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,32 +1,19 @@
 import numpy as np
 
-def calculate(list):
-    array_as=np.asarray(list)
-    arrnum=array_as.reshape(3,3)
-    np.set_printoptions(precision=16, suppress=False)
-
-    meanax0=arrnum.mean(axis=0)
-    meanax1=arrnum.mean(axis=1)
-    meanax=arrnum.mean()
-
-    varax0=arrnum.var(axis=0)
-    varax1=arrnum.var(axis=1)
-    varax=arrnum.var()
-
-    stdax0=arrnum.std(axis=0)
-    stdax1=arrnum.std(axis=1,dtype=np.double)
-    stdax=arrnum.std()
+def calculate(input_list):
+    if len(input_list) != 9:
+        raise ValueError("List must contain nine numbers.")
     
-    minax0=arrnum.min(axis=0)
-    minax1=arrnum.min(axis=1)
-    minax=arrnum.min()
-
-    maxax0=arrnum.max(axis=0)
-    maxax1=arrnum.max(axis=1)
-    maxax=arrnum.max()
-
-    sumax0=arrnum.sum(axis=0)
-    sumax1=arrnum.sum(axis=1)
-    sumax=arrnum.sum()    
-    calculations =f" {{ \n'mean': [{meanax0}, {meanax1}, {meanax}],\n'variance': [{varax0}, {varax1}, {varax}],\n'standard deviation': [{stdax0}, {stdax1}, {stdax}],\n 'max': [{maxax0}, {maxax1}, {maxax}],\n'min': [{minax0}, {minax1}, {minax}],\n'sum': [{sumax0}, {sumax1},{sumax}] \n}}"
+    arrnum = np.array(input_list).reshape(3, 3)
+    np.set_printoptions(precision=16, suppress=False)
+    
+    calculations = {
+        'mean': [list(np.mean(arrnum, axis=0)), list(np.mean(arrnum, axis=1)), np.mean(arrnum).item()],
+        'variance': [list(np.var(arrnum, axis=0)), list(np.var(arrnum, axis=1)), np.var(arrnum).item()],
+        'standard deviation': [list(np.std(arrnum, axis=0)), list(np.std(arrnum, axis=1)), np.std(arrnum).item()],
+        'max': [list(np.max(arrnum, axis=0)), list(np.max(arrnum, axis=1)), np.max(arrnum).item()],
+        'min': [list(np.min(arrnum, axis=0)), list(np.min(arrnum, axis=1)), np.min(arrnum).item()],
+        'sum': [list(np.sum(arrnum, axis=0)), list(np.sum(arrnum, axis=1)), np.sum(arrnum).item()]
+    }
+    
     return calculations
